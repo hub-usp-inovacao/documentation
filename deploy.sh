ebca013fa4d147e2e4af52ff127506f506fbc458
Thiago Guerrero <tgbalera@hotmail.com>
2021-09-22 16:40:40 -0300

    attempt to fix deploy
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,14 @@
 # abort on errors
 set -e
 
+# install dependencies
+yarn install
+
 # build
 yarn build
 
 # navigate into the build output directory
-cd .vuepress/dist
+cd src/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
